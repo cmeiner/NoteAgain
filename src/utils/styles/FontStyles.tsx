f231cfcaf4310f0524d915d792883a0cc74650bf
miMeiner <christian.meiner@medieinstitutet.se>
2022-12-21 16:48:24 +0100

    Add react-import in FontStyles.tsx

--- src/utils/styles/FontStyles.tsx
+++ src/utils/styles/FontStyles.tsx
@@ -1,5 +1,5 @@
 import { useFonts } from 'expo-font';
-import { FC, ReactNode } from 'react';
+import React, { FC, ReactNode } from 'react';
 import { StyleSheet, Text } from 'react-native';
 
 type Props = {
