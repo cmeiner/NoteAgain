340472639ba2567c5523fa981958af69dbf11af0
felixloot <felix.bakkum2002@gmail.com>
2022-12-21 12:56:28 +0100

    Added "Better Comments"
    Added comments to our data types.
    "!" Is what the type is including from other types.

--- types/FirebaseTypes.ts
+++ types/FirebaseTypes.ts
@@ -1,46 +1,76 @@
-type Reminder = {
-    id: string;
-    title: string;
-    description: string;
-    creator: string;
-    remindAt: string;
-    sharedWith: Share[ ];
-}
-type TodoList = {
-    id: string;
-    description?: string;
-    items: Todo [ ];
-    creator: string;
-    sharedWith: Share[ ];
-}
-    
-type Todo = {
-    id: string;
-    description: string;
-    completed: boolean;
-}
-    
+/**
+ * * Status of shared reminders
+ * ! pending
+ * ! accepted
+ * ! declined
+ */
 type Status = "pending" | "accepted" | "declined";
 
+/**
+ * * TodoList Data
+ * ! Todo Data
+ * ! Share Data
+ */
+type TodoList = {
+  id: string;
+  description?: string;
+  items: Todo[];
+  creator: string;
+  sharedWith: Share[];
+};
+
+/**
+ * * Todo Data
+ */
+type Todo = {
+  id: string;
+  description: string;
+  completed: boolean;
+};
+
+/**
+ * * Share Data
+ * ! Status
+ */
 type Share = {
-    senderID: string; 
-    receiverID: string;
-    reminderType: string;
-    status: Status;
-}
-        
+  senderID: string;
+  receiverID: string;
+  reminderType: string;
+  status: Status;
+};
+
+/**
+ * * Reminder Data
+ * ! Share Data
+ */
+type Reminder = {
+  id: string;
+  title: string;
+  description: string;
+  creator: string;
+  remindAt: string;
+  sharedWith: Share[];
+};
+
+/**
+ * * User Data
+ * ! Share Data
+ * ! TodoList Data
+ * ! Reminder Data
+ *
+ */
 type User = {
-    id: string;
-    email: string;
-    displayName: string;
-    reminders: Reminder [ ];
-    todos: TodoList [ ];
-    receivedItems: {
-        todos: TodoList [ ];
-        reminders: Reminder [ ];
-    };
-    sharedItems: {
-        todos: TodoList [ ];
-        reminders: Reminder [ ];
-    }
-}
+  id: string;
+  email: string;
+  displayName: string;
+  reminders: Reminder[];
+  todos: TodoList[];
+  receivedItems: {
+    todos: TodoList[];
+    reminders: Reminder[];
+  };
+  sharedItems: {
+    todos: TodoList[];
+    reminders: Reminder[];
+  };
+};
