60f809def0e30e4d6b7320880ceb4108c4916de1
felixloot <felix.bakkum2002@gmail.com>
2022-12-29 09:18:56 +0100

    Stay Signed In
    Created conditonal routing if you are signed in.

--- App.tsx
+++ App.tsx
@@ -6,7 +6,7 @@ import {
 } from '@expo-google-fonts/sora';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { auth } from './config/firebaseConfig';
 import { loginUser } from './hooks/firebase/UserHooks';
 import {
@@ -19,6 +19,18 @@ import { NavBar } from './src/components/NavBar';
 
 const App = () => {
   const [loggedIn, setLoggedIn] = useState(false);
+  useEffect(() => {
+    checkUserData().then((boolean) => {
+      if (boolean) {
+        //console.log('Inloggad');
+        getUserData().then((data) => {
+          //console.log(data);
+          loginUser(data);
+          setLoggedIn(true);
+        });
+      }
+    });
+  }, [loggedIn]);
   const [fontsLoaded] = useFonts({
     Sora_700Bold,
     Sora_400Regular,
@@ -29,33 +41,30 @@ const App = () => {
     return null;
   }
 
-  resetUserData();
-
-  checkUserData().then((boolean) => {
-    if (boolean) {
-      console.log('Inloggad');
-      getUserData().then((data) => {
-        loginUser(data);
-        setLoggedIn(true);
-        console.log(loggedIn);
-      });
-    }
-  });
+  // resetUserData();
 
   const Stack = createNativeStackNavigator();
-  console.log(auth.currentUser);
+  //console.log(auth.currentUser);
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName={loggedIn ? 'Login' : 'Navbar'}>
+      <Stack.Navigator>
+        {loggedIn ? (
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name="NavBar"
+            component={NavBar}
+          />
+        ) : (
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name="Login"
+            component={Login}
+          />
+        )}
         <Stack.Screen
           options={{ headerShown: false }}
-          name="NavBar"
+          name="HomeScreen"
           component={NavBar}
-        />
-        <Stack.Screen
-          options={{ headerShown: false }}
-          name="Login"
-          component={Login}
         />
       </Stack.Navigator>
     </NavigationContainer>
