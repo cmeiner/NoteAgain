e31b6dfa3db1d1f9446f0b2e22e8834b0ee06906
AdaHep <adam.hepsever@gmail.com>
2022-12-29 10:42:27 +0100

    new reminder "complete" still needs some styling. Modal also needs styling still

--- pages/StartPage.tsx
+++ pages/StartPage.tsx
@@ -74,7 +74,6 @@ const StartPage = ({ navigation }: any) => {
             transparent={true}
             visible={modalVisible}
             onRequestClose={() => {
-              Alert.alert('Modal has been closed.');
               setModalVisible(!modalVisible);
             }}
           >
